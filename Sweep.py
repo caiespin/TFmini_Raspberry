a692ceccd923f2896895e35875c0288168a94eb0
Carlos Espinosa <caiespin@ucsc.edu>
2020-02-13 19:48:46 -0800

    Increasing sweep time

--- Sweep.py
+++ Sweep.py
@@ -35,11 +35,11 @@ def loop():
     while True:
         for dc in range(0, 181, 1):   # make servo rotate from 0 to 180 deg
             servoWrite(dc)     # Write dc value to servo
-            time.sleep(0.001)
+            time.sleep(0.01)
         time.sleep(0.5)
         for dc in range(180, -1, -1): # make servo rotate from 180 to 0 deg
             servoWrite(dc)
-            time.sleep(0.001)
+            time.sleep(0.01)
         time.sleep(0.5)
 
 def destroy():
